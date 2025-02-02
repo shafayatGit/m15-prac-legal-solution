0f667b58674bb4e1f244bda69eae802c3196b61d
shafayatGit <shafayathossain.drmc@gmail.com>
2025-02-02 23:45:54 +0600

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage:{
+            blackOverlay: "linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,0,8) 100%))"
+        },
+      },
     },
     plugins: [],
   }
